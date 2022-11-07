c01269521700cb9875978bf475958d48f6f57feb
Gautam Vaishnav <gauravvaishnav8690@gmail.com>
2022-11-07 20:49:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="btn">h</h1>
+      <div className="text-center">
+        <h1 className="btn">h</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta iusto
+          necessitatibus distinctio. Facere nisi aut nihil quas doloribus fugit
+          error est voluptas expedita ducimus, dolorum debitis sapiente
+          similique hic magni!
+        </p>
+      </div>
     </>
   );
 }
